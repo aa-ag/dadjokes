f12fafee8b5183f6d9d74b64b2bd3ce7d5967426
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-02 21:29:02 -0500

    added logic to fetch/request/get joke from api

--- scrip.py
+++ scrip.py
@@ -4,9 +4,17 @@ import requests
 
 ############------------ FUNCTIONS ------------############
 def get_random_dad_joke():
-    pass
+    joke = requests.get('https://icanhazdadjoke.com',
+                        headers={'Accept': 'application/json'}).json()
+
+    print(joke)
 
 
 ############------------ DRIVER CODE ------------############
 if __name__ == "__main__":
     get_random_dad_joke()
+    '''
+     {'id': 'FdN7hiG6Uvc', 
+     'joke': "Why can't eggs have love? They will break up too soon.", 
+     'status': 200}
+    '''
